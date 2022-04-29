b41db2933af6c0253dc5463d8c340ff52d23773f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2022-04-29 13:41:06 +0900

    [Silver I] Title: 포도주 시식, Time: 136 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.kt
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.kt
@@ -1,6 +1,5 @@
-package boj.doing
-
 import java.io.*
+import java.lang.Integer.max
 import java.util.*
 
 internal val scan = FastReader()
@@ -9,7 +8,10 @@ internal val bw = BufferedWriter(OutputStreamWriter(System.out))
 
 internal var N: Int = 0
 //dp[i][k] k = 0, 1 -> i번 와인을 시식했을때, 시식 하지 않았을때 최대 양
-lateinit var dp: Array<IntArray>
+//lateinit var dp: Array<IntArray>
+
+// i번째 까자 골랐을때 와인 시식 최대로
+lateinit var dp: IntArray
 lateinit var arr: IntArray
 
 fun main() {
@@ -24,7 +26,8 @@ fun solve() {
 fun input() {
     N = scan.nextInt()
     arr = IntArray(N+1)
-    dp = Array(N+1) { IntArray(2) }
+    //dp = Array(N+1) { IntArray(2) }
+    dp = IntArray(N+1)
 
     for(i in 1..N) {
         arr[i] = scan.nextInt()
@@ -38,29 +41,31 @@ fun solution() {
     // 가짜 문제를 정의하는데 성공했지만 점화식을 성공적으로 세우지 못함..
     for (i in 3..N) {
         //dp[i][0] = Math.max(dp[i - 2][0] + arr[i], dp[i - 2][1] + arr[i])
-        dp[i][0] = (dp[i - 2][0] + arr[i]).coerceAtLeast(dp[i - 2][1] + arr[i])
+//        dp[i][0] = (dp[i - 2][0] + arr[i]).coerceAtLeast(dp[i - 2][1] + arr[i])
         //dp[i][0] = Math.max(dp[i][0], Math.max(dp[i - 3][0], dp[i - 3][1]) + arr[i])
-        dp[i][0] = dp[i][0].coerceAtLeast(dp[i - 3][0].coerceAtLeast(dp[i - 3][1]) + arr[i])
+//        dp[i][0] = dp[i][0].coerceAtLeast(dp[i - 3][0].coerceAtLeast(dp[i - 3][1]) + arr[i])
 
-        dp[i][1] = dp[i - 1][0] + arr[i]
+//        dp[i][1] = dp[i - 1][0] + arr[i]
+        dp[i] = max(dp[i-3] + arr[i] + arr[i-1], dp[i-2] + arr[i])
+        dp[i] = max(dp[i-1], dp[i])
+        
     }
     println(
 //        Math.max(
 //            Math.max(dp[N][0], dp[N][1]),
 //            Math.max(dp[N - 1][0], dp[N - 1][1])
 //        )
-        dp[N][0].coerceAtLeast(dp[N][1]).coerceAtLeast(dp[N - 1][0].coerceAtLeast(dp[N - 1][1]))
+//        dp[N][0].coerceAtLeast(dp[N][1]).coerceAtLeast(dp[N - 1][0].coerceAtLeast(dp[N - 1][1]))
+        dp[N]
     )
 }
 
 fun preprocess() {
     //초기값
-    dp[1][0] = 0
-    dp[1][1] = arr[1]
+    dp[1] = arr[1]
 
     if (N >= 2) {
-        dp[2][0] = arr[2]
-        dp[2][1] = dp[1][1] + arr[2]
+        dp[2] = dp[1]+ arr[2]
     }
 }
 
