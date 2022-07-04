5e494003200116ffff9040c8e0468dae8b8cd6fb
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2022-07-04 10:25:54 +0900

    [Silver II] Title: 최소 힙, Time: 1488 ms, Memory: 40500 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -0,0 +1,82 @@
+import java.io.*
+import java.util.*
+
+val scan = FastReader()
+val sb = StringBuilder()
+
+val q = PriorityQueue<Int>()
+
+var N = 0
+
+fun main() {
+    input()
+    pro()
+}
+
+fun input() {
+    N = scan.nextInt()
+}
+
+fun pro() {
+    for (i in 0 until N) {
+        val input = scan.nextInt()
+
+        if(input == 0) {
+            if(!q.isEmpty()) {
+                println(q.poll())
+            }
+            else {
+                println(0)
+            }
+        }
+        else {
+            q.add(input)
+        }
+    }
+}
+
+class FastReader {
+    var br: BufferedReader
+    var st: StringTokenizer? = null
+
+    constructor() {
+        br = BufferedReader(InputStreamReader(System.`in`))
+    }
+
+    constructor(s: String?) {
+        br = BufferedReader(FileReader(File(s)))
+    }
+
+    operator fun next(): String {
+        while (st == null || !st!!.hasMoreElements()) {
+            try {
+                st = StringTokenizer(br.readLine())
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return st!!.nextToken()
+    }
+
+    fun nextInt(): Int {
+        return next().toInt()
+    }
+
+    fun nextLong(): Long {
+        return next().toLong()
+    }
+
+    fun nextDouble(): Double {
+        return next().toDouble()
+    }
+
+    fun nextLine(): String {
+        var str = ""
+        try {
+            str = br.readLine()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return str
+    }
+}
