5bb67e41cd97faac447088257354e0d8f0cfbc11
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2022-07-29 15:05:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int d[11];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+
+	cin >> t;
+	d[0] = 1;
+
+	for (int i = 1; i <= 10; i++) {
+		if (i - 1 >= 0) {
+			d[i] += d[i - 1];
+		}
+		if (i - 2 >= 0) {
+			d[i] += d[i - 2];
+		}
+		if (i - 3 >= 0) {
+			d[i] += d[i - 3];
+		}
+	}
+
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << d[n] << '\n';
+	}
+}
