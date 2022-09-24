92051d667ce2915ba20b432e4d8c14a05135a56e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2022-09-24 22:47:05 +0900

    [Gold IV] Title: 테트로미노, Time: 224 ms, Memory: 119700 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,165 +1,53 @@
-# 일반적으로 for문으로 푸는 것이 재귀보다 성능이 좋기 때문에 dfs로 풀어도 좋지만 for문으로 푸는 연습도 해야한다.
-
 import sys
 
+si = sys.stdin.readline
 
-def si():
-    return sys.stdin.readline().rstrip()
+# 최댓값에서의 커팅 방법
 
-
-# 모든 테트로미노의 경우의수를 모든 칸에 넣어서 확인해야하나
-# ㅇㅇ 각 테트로미노 마다 경우의 수 총 19가지를 미리 만들어놔야 -> 그대로 놓고 한 칸 씩 옮겨 가면서 비교
-shapes = [
-    # 1
-    [[1, 1, 1, 1],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 0, 0, 0],
-     [1, 0, 0, 0],
-     [1, 0, 0, 0],
-     [1, 0, 0, 0]],
-
-    # 정사각형
-    [[1, 1, 0, 0],
-     [1, 1, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    # ㄱ 자
-    [[1, 0, 0, 0],
-     [1, 0, 0, 0],
-     [1, 1, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 1, 0, 0],
-     [0, 1, 0, 0],
-     [1, 1, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 1, 1, 0],
-     [1, 0, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 0, 0, 0],
-     [1, 1, 1, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 1, 1, 0],
-     [0, 0, 1, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 0, 1, 0],
-     [1, 1, 1, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 1, 0, 0],
-     [0, 1, 0, 0],
-     [0, 1, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 1, 0, 0],
-     [1, 0, 0, 0],
-     [1, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    # 번개
-    [[1, 0, 0, 0],
-     [1, 1, 0, 0],
-     [0, 1, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 1, 0, 0],
-     [1, 1, 0, 0],
-     [1, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 1, 0, 0],
-     [0, 1, 1, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 1, 1, 0],
-     [1, 1, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    # ㅗ
-    [[1, 1, 1, 0],
-     [0, 1, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 1, 0, 0],
-     [1, 1, 1, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[1, 0, 0, 0],
-     [1, 1, 0, 0],
-     [1, 0, 0, 0],
-     [0, 0, 0, 0]],
-
-    [[0, 1, 0, 0],
-     [1, 1, 0, 0],
-     [0, 1, 0, 0],
-     [0, 0, 0, 0]],
-]
-
-# def get_max_sum(x, y):
-#     is_possible = True
-#     max_sum = 0
-#     for i in range(19):
-#         sum_of_nums = 0
-#         for dx in range(4):
-#             for dy in range(4):
-#                 if shapes[i][dx][dy] == 0:
-#                     continue
-#                 if x + dx >= n or y + dy >= m:
-#                     is_possible = False
-#                     break
-#                 else:
-#                     sum_of_nums += grid[x + dx][y + dy]
-#             if not is_possible:
-#                 break
-#         if is_possible:
-#             max_sum = max(max_sum, sum_of_nums)
-#     return max_sum
-
-def get_max_sum(x, y):
-    max_sum = 0
-    for i in range(19):
-        sum_of_nums = get_sum_of_nums(i, x, y)
-        max_sum = max(max_sum, sum_of_nums)
-    return max_sum
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
 
 
-def get_sum_of_nums(i, x, y):
-    sum_of_nums = 0
-    for dx in range(4):
-        for dy in range(4):
-            if shapes[i][dx][dy] == 0:
-                continue
-            if x + dx >= n or y + dy >= m:
-                return 0
-            else:
-                sum_of_nums += grid[x + dx][y + dy]
+def can_go(x, y):
+    return in_range(x, y) and not visited[x][y]
 
-    return sum_of_nums
+
+def get_max_sum(x, y, idx, sum):
+    global max_sum
+
+    if max_sum >= sum + max_val * (3 - idx):
+        return
+
+    if idx == 3:
+        max_sum = max(max_sum, sum)
+        return
+
+    else:
+        for dx, dy in zip(dxs, dys):
+            nx = x + dx
+            ny = y + dy
+            if can_go(nx, ny):
+                if idx == 1:
+                    visited[nx][ny] = True
+                    get_max_sum(x, y, idx + 1, sum + grid[nx][ny])
+                    visited[nx][ny] = False
+                visited[nx][ny] = True
+                get_max_sum(nx, ny, idx + 1, sum + grid[nx][ny])
+                visited[nx][ny] = False
 
 
 n, m = map(int, si().split())
 grid = [list(map(int, si().split())) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
 
-ans = 0
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+max_sum = 0
+max_val = max(map(max, grid))
 
-for i in range(n):
-    for j in range(m):
-        ans = max(ans, get_max_sum(i, j))
+for x in range(n):
+    for y in range(m):
+        visited[x][y] = True
+        get_max_sum(x, y, 0, grid[x][y])
+        visited[x][y] = False
 
-print(ans)
+print(max_sum)
