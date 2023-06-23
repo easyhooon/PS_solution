0c0189f9264a6d41ace24714a3c1215c684d5497
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-24 04:13:19 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 6652 ms, Memory: 513696 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.py
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.py
@@ -1,37 +1,43 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+si = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-MAX = float('inf')
+# dxs = [-1, 0, 1, 0]
+# dys = [0, -1, 0, 1]
 
-board = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[[MAX]*(K+1) for _ in range(M)] for _ in range(N)]
-visited[0][0][K] = 0
-q = deque([(0, 0, 1, K)])
-result = MAX
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-while q :
-  x, y, t, left = q.popleft()
-  if (x, y) == (M-1, N-1) :
-    result = min(result, t)
-    continue
+INT_MAX = sys.maxsize
 
-  daytime = t % 2
-  for k in range(4) :
-    ax, ay = x + dx[k], y + dy[k]
-    if -1 < ax < M and -1 < ay < N :
-      if board[ay][ax] == 0 and visited[ay][ax][left] > t:
-        visited[ay][ax][left] = t
-        q.append((ax, ay, t+1, left))
-      if board[ay][ax] == 1 and left and visited[ay][ax][left-1] > t :
-        if daytime : 
-          visited[ay][ax][left-1] = t
-          q.append((ax, ay, t+1, left-1))
-        else :
-          q.append((x, y, t+1, left))
+n, m, k = map(int, si().split())
+board = [list(map(int, si().strip())) for _ in range(n)]
+visited = [[[INT_MAX] * (k + 1) for _ in range(m)] for _ in range(n)]
+# 낮 1, 밤 0
+q = deque([(0, 0, 1, k)])
 
-print(result if result < MAX else -1)
+result = INT_MAX
+
+while q:
+    x, y, t, left = q.popleft()
+
+    if (x, y) == (n - 1, m - 1):
+        result = min(result, t)
+        continue
+
+    daytime = t % 2
+    for k in range(4):
+        nx, ny = x + dx[k], y + dy[k]
+        if 0 <= nx < n and 0 <= ny < m:
+            if board[nx][ny] == 0 and visited[nx][ny][left] > t:
+                visited[nx][ny][left] = t
+                q.append((nx, ny, t + 1, left))
+            if board[nx][ny] == 1 and left and visited[nx][ny][left - 1] > t:
+                if daytime:
+                    visited[nx][ny][left - 1] = t
+                    q.append((nx, ny, t + 1, left - 1))
+                else:
+                    q.append((x, y, t + 1, left))
+
+print(result if result != INT_MAX else -1)
