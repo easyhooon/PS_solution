d9f1dd8b5c4aeacdabff9bfc395e28a68677dd77
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-24 00:51:37 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,79 +1,82 @@
 import sys
 
-# x, y 방향 지정
-# 맵 안에 1 -> 사과, 0 -> 빈칸, -1 -> 뱀
+# 1 -> 사과,
+# 0 -> 빈칸,
+# -1 -> 뱀
 
-# 방향 시계방향 (오른쪽으로 90도씩 회전 배열)
-# 행과 열을 명시하였으므로 map[y][x]의 형태가 안헷갈릴것같다..
-# 초기 조건에 오른쪽을 향한다라고 되어있으므로
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
+# 초기 조건에서 오른쪽으로 향하고 있음
+si = sys.stdin.readline
 
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-def si():
-    return sys.stdin.readline().rstrip()
-
-
+# 보드의 크기 <= 100
 n = int(si())
+# 사과의 개수 <= 100
 k = int(si())
 
-grid = [[0 for _ in range(101)] for _ in range(101)]
-cmd = [0] * 10001
-snake_y = [0] * 10101
-snake_x = [0] * 10101
+board = [[0 for _ in range(101)] for _ in range(101)]
+cmd = [0] * 10011
+
+snake_x = [0] * 10011
+snake_y = [0] * 10011
 
 for _ in range(k):
-    y, x = map(int, si().split())
-    grid[y][x] = 1
+    x, y = map(int, si().split())
+    board[x][y] = 1
 
+# 뱀의 방향 변환 횟수 <= 100
 l = int(si())
+
 for _ in range(l):
     time, c = map(str, si().split())
     cmd[int(time)] = c
 
+# 시간
 time = 0
 # 방향
-dir = 0
-# 처음 위치로 초기화
-head_y, head_x, tail_index = 1, 1, time
-snake_y[time], snake_x[time], = head_y, head_x,
-# 현재 해당 칸에 뱀이 들어가 있는 경우 -1
-grid[head_y][head_x] = -1
+dir = 3
+
+# 처음 위치 초기화
+head_x, head_y, tail_index = 1, 1, time
+#
+snake_x[time], snake_y[time] = head_x, head_y
+# 현재 해당 칸에 뱀이 들어가 있는 경우 -> -1
+board[head_x][head_y] = -1
 
 while True:
+    # 시간 진행
     time += 1
 
-    head_y += dy[dir]
+    # 머리 좌표 현재 방향에 맞춰 갱신
     head_x += dx[dir]
+    head_y += dy[dir]
 
-    # 맵을 벗어나거나 #뱀의 머리가 뱀의 몸통에 닿거나
-    if head_y < 1 or head_y > n or head_x < 1 or head_x > n or grid[head_y][head_x] == -1:
+    # 맵을 벗어나거나, 뱀의 머리가 뱀의 몸통에 닿을 경우
+    if (head_x < 1 or head_x > n or head_y < 1 or head_y > n) or board[head_x][head_y] == -1:
         break
 
-    snake_y[time] = head_y
     snake_x[time] = head_x
+    snake_y[time] = head_y
 
-    # 빈칸 (그냥 진행)
-    if grid[head_y][head_x] == 0:
-        tail_y = snake_y[tail_index]
+    # 진행
+    if board[head_x][head_y] == 0:
         tail_x = snake_x[tail_index]
-        # 꼬리좌표 지우기 (빈칸으로)
-        grid[tail_y][tail_x] = 0
+        tail_y = snake_y[tail_index]
+
+        # 꼬리 좌표 제거
+        board[tail_x][tail_y] = 0
         tail_index += 1
 
-    # 뱀의 머리좌표 진행
-    grid[head_y][head_x] = -1
+    # 뱀의 머리 좌표 진행
+    board[head_x][head_y] = -1
 
-    # 왼쪽으로 90도 방향 회전
-    # 시계방향으로 증가하는 순이므로 3칸 증가 -> 왼쪽(90도 세번)
-    # (dir -1) 로도 표현할 수 있으나 음수가 될 수 있음
+    # 왼쪽으로 90 도 방향 회전
     if cmd[time] == 'L':
-        dir = (dir + 3) % 4
-
+        dir = (dir + 1) % 4
 
     # 오른쪽으로 90도 방향 회전
-    # 시계방향으로 증가하는 순이므로 1칸 증가 -> 오른쪽
     if cmd[time] == 'D':
-        dir = (dir + 1) % 4
+        dir = (dir + 3) % 4
 
 print(time)
