0a8162825127b4f18e3d444c7cb5712e05644886
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-06 21:06:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,39 +1,47 @@
 import heapq
 
+# len(operations) <= 1,000,000
+# 큐가 비어있으면 [0,0] 을 return 
 def solution(operations):
-    min_pq = []
+    answer = []
+    
     max_pq = []
-
+    min_pq = []
+    
+    # O(n) or O(nlogn) 으로 풀어야 함 
     for op in operations:
         if op.startswith('I'):
             num = int(op.split(' ')[1])
-            heapq.heappush(max_pq, (-num, num))
+            heapq.heappush(max_pq, -num)
             heapq.heappush(min_pq, num)
-
+            
         elif op.startswith('D'):
             # "-1" 이거 int 씌우면 -1 되나 -> 됨
             num = int(op.split(' ')[1])
             # 최댓값 삭제
             if num == 1:
-                if max_pq:
-                    max_value = heapq.heappop(max_pq)[1]
-                    min_pq.remove(max_value)
-
+                if max_pq and min_pq:
+                    heapq.heappop(max_pq)
+                    if not max_pq:
+                        min_pq = []
+                    
+                
             # 최솟값 삭제
             else:
-                if min_pq:
-                    min_value = heapq.heappop(min_pq)
-                    max_pq.remove((-min_value, min_value))
+                if min_pq and min_pq:
+                    heapq.heappop(min_pq)
+                    if not min_pq:
+                        max_pq = []
+                
         # print(max_pq)
         # print(min_pq)
         # print()
-
+            
     if max_pq and min_pq:
-        answer = [max_pq[0][1], min_pq[0]]
-
+        answer = [-max_pq[0], min_pq[0]]
+        
     else:
         answer = [0, 0]
-
+        
+    
     return answer
-
-# print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
