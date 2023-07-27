925e52f2c08a8af320eee13a80edc8b9723fb039
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-07-27 18:12:33 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 276 ms, Memory: 57508 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,62 @@
+# 최소 비용 구하기 2
+
+# 최소 비용
+# 최소 비용을 갖는 경로에 포함되어있는 도시의 개수(출발, 도착 도시 포함
+# 경로를 방문하는 도시 순서대로 출력
+
+import sys
+import heapq
+
+si = sys.stdin.readline
+
+
+def dijkstra(start, dist, edges):
+    pq = []
+    heapq.heappush(pq, (0, start))
+    dist[start] = 0
+
+    while pq:
+        min_dist, min_idx = heapq.heappop(pq)
+
+        if dist[min_idx] != min_dist:
+            continue
+
+        for target_idx, target_dist in edges[min_idx]:
+            new_dist = dist[min_idx] + target_dist
+            if dist[target_idx] > new_dist:
+                dist[target_idx] = new_dist
+                path[target_idx] = min_idx
+                heapq.heappush(pq, (new_dist, target_idx))
+
+    return dist
+
+
+n = int(si())
+m = int(si())
+
+dist = [sys.maxsize for _ in range(n + 1)]
+edges = [[] for _ in range(n + 1)]
+path = [0] * (n + 1)
+
+for _ in range(m):
+    x, y, w = map(int, si().split())
+    edges[x].append((y, w))
+
+# 출발점의 도시번호, 도착점의 도시번호가 주어짐 -> 다익스트라
+start, end = map(int, si().split())
+
+dist = dijkstra(start, dist, edges)
+print(dist[end])
+
+x = end
+nodes = []
+nodes.append(end)
+
+while x != start:
+    x = path[x]
+    nodes.append(x)
+
+print(len(nodes))
+
+for node in nodes[::-1]:
+    print(node, end = " ")
