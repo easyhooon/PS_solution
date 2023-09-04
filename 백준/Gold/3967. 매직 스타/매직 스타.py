6dd7e3cad8a2e7eb1c553425b521dadfc5786809
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-09-04 16:43:25 +0900

    [Gold V] Title: 매직 스타, Time: 1532 ms, Memory: 122780 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.py
+++ 백준/Gold/3967. 매직 스타/매직 스타.py
@@ -11,28 +11,30 @@ positions = [
     (3, 1), (3, 3), (3, 5), (3, 7),
     (4, 4)
 ]
+lines = [
+    [positions[0], positions[2], positions[5], positions[7]],
+    [positions[0], positions[3], positions[6], positions[10]],
+    [positions[1], positions[2], positions[3], positions[4]],
+    [positions[7], positions[8], positions[9], positions[10]],
+    [positions[11], positions[9], positions[6], positions[4]],
+    [positions[11], positions[8], positions[5], positions[1]]
+]
 filled = [False] * 12
 
 
 def get_val(ch):
-    if ch == 'x':
-        return 0
     return ord(ch) - ord('A') + 1
 
 
 def validate():
-    lines = [
-        [positions[0], positions[2], positions[5], positions[7]],
-        [positions[0], positions[3], positions[6], positions[10]],
-        [positions[1], positions[2], positions[3], positions[4]],
-        [positions[7], positions[8], positions[9], positions[10]],
-        [positions[11], positions[9], positions[6], positions[4]],
-        [positions[11], positions[8], positions[5], positions[1]]
-    ]
-
     for line in lines:
-        if sum([get_val(board[pos[0]][pos[1]]) for pos in line]) != 26:
+        line_sum = 0
+        for pos in line:
+            line_sum += get_val(board[pos[0]][pos[1]])
+
+        if line_sum != 26:
             return False
+
     return True
 
 
@@ -63,6 +65,5 @@ for i in range(5):
     for j in range(9):
         if 'A' <= board[i][j] <= 'L':
             filled[ord(board[i][j]) - ord('A')] = True
-# print(filled)
 
 dfs(0)
